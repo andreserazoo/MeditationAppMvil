2afdadf4df0781602ffcaa1fd2de77ab25161dd2
Jorge Andres Erazo Sanchez <erazoandres24@gmail.com>
2025-03-13 09:35:36 -0500

    AlineaImagen

--- app/src/main/java/edu/unicauca/meditationapp/MainActivity.kt
+++ app/src/main/java/edu/unicauca/meditationapp/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +13,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -20,10 +25,14 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import edu.unicauca.meditationapp.ui.theme.MeditationappTheme
 
@@ -42,6 +51,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 
 fun BarraBusqueda(
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
     modifier: Modifier = Modifier
 ){
     TextField(
@@ -73,27 +84,48 @@ fun BarraBusqueda(
 @Preview
 @Composable
 fun PreviewBarraDeBusqueda(){
-    BarraBusqueda()
+    BarraBusqueda(
+        text = R.string.search,
+        modifier = Modifier.height(56.dp),
+        drawable = TODO()
+    )
 }
 
 @Composable
 fun CuadroAlineaTuCuerpo(
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
     modifier: Modifier = Modifier
 ){
     Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
     ){
+
         Image(
             painter = painterResource(R.drawable.ab1_inversions),
-            contentDescription = stringResource(R.string.alinea_tu_cuerpo_txt)
+            contentDescription = stringResource(R.string.alinea_tu_cuerpo_txt),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(100.dp)
+                .clip(CircleShape)
         )
         Text(
-            text =(stringResource(R.string.ab1_inversions_txt)))
+            text =(stringResource(R.string.ab1_inversions_txt)),
+            modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp),
+            style = MaterialTheme.typography.bodyMedium
+
+        )
+
     }
 
 }
 @Preview
 @Composable
 fun PreviewCuadroAlineaTuCuerpo(){
-    CuadroAlineaTuCuerpo()
+    CuadroAlineaTuCuerpo(
+        text = R.string.alinea_tu_cuerpo_txt,
+        drawable = R.drawable.ab1_inversions,
+        modifier = Modifier.padding(8.dp)
+    )
 }
